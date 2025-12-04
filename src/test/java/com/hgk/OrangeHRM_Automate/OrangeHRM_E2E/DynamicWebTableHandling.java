f8e035d1e03925d01846a0945a8d3c086029ebae
HariGovind00 <govindharik@gmail.com>
2025-12-04 12:42:43 +0530

    Login Test Method UPdate

---
+++ src/test/java/com/hgk/OrangeHRM_Automate/OrangeHRM_E2E/DynamicWebTableHandling.java
@@ -0,0 +1,40 @@
+package com.hgk.OrangeHRM_Automate.OrangeHRM_E2E;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DynamicWebTableHandling 
+{
+	WebDriver driver;
+	@Test
+public void webTableHandle()
+{
+		String browser="Chrome";
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://practice.expandtesting.com/Dynamic-table");
+		List<WebElement> firstColumn=driver.findElements(By.xpath("//div[contains(@class,'table')]//tr//td[1]"));
+//		WebElement ele=driver.findElement(By.xpath("//td[text()='Chrome']/following-sibling::td[contains(text(),'%')]"));
+//		System.out.println(ele.getText());
+		for(int i=0;i<firstColumn.size();i++)
+		{
+			
+			if(firstColumn.get(i).getText().equalsIgnoreCase(browser))
+			{
+				//System.out.println(firstColumn.get(i));
+				WebElement ele=driver.findElement(By.xpath("//td[text()='"+browser+"']/following-sibling::td[contains(text(),'%')]"));
+	
+				System.out.println(ele.getText());
+				
+			
+			}
+		}
+}
+}
--- src/test/java/com/hgk/OrangeHRM_Automate/OrangeHRM_E2E/TestRunBaseClass.java
+++ src/test/java/com/hgk/OrangeHRM_Automate/OrangeHRM_E2E/TestRunBaseClass.java
@@ -11,14 +11,16 @@ import orangeHRM_E2E.BrowserInIt.BrowserInItPage;
 public class TestRunBaseClass extends Action
 
 {
+	//Login Page Object Creation.
 	LoginPage login=new LoginPage();
 	BrowserInItPage browser=new BrowserInItPage();
 	SearchPage page=new SearchPage();
 @Test(retryAnalyzer = RetryBeforeFailing.class)
 public void loginPageTest() throws Exception
 {
-	browser.browser();
 	
+	browser.browser();
+	System.out.println("Welcome to login page test method.");
 	login.loginPage("Admin","admin123");
 	page.searchPage("PIM");
 }
